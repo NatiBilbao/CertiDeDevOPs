ceb269719b547071f53e39f91be0131c4153ba9a
NatyBilbao <natabilbaocano18@gmail.com>
2023-10-24 10:37:43 -0400

    Tarea

---
+++ src/MiComponente.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import MiComponente from './MiComponente';
+
+test('Renderiza el componente MiComponente correctamente', () => {
+  // Renderiza el componente en un contenedor virtual
+  const { getByText, getByTestId } = render(<MiComponente texto="Texto de prueba" />);
+
+  // Realiza aserciones sobre el contenido del componente
+  expect(getByTestId('mi-componente')).toBeInTheDocument();
+  expect(getByText('Mi Componente')).toBeInTheDocument();
+  expect(getByText('Texto de prueba')).toBeInTheDocument();
+});
+
+test('Verifica el contenido del componente sin propiedades', () => {
+  const { getByText } = render(<MiComponente />);
+
+  expect(getByText('Mi Componente')).toBeInTheDocument();
+  expect(getByText('')).toBeInTheDocument(); // El texto está vacío si no se proporciona la propiedad 'texto'
+});
