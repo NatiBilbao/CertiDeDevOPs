9670163992e60752d48a145e18f49bbb3a9fdad5
NatyBilbao <natabilbaocano18@gmail.com>
2023-10-24 01:18:22 -0400

    Tarea

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Mi Primer Proyecto React</h1>
+      <p>Bienvenido a React.</p>
+    </div>
+  );
+}
+
+export default App;
